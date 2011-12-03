0fe7d7443b827add879720405a98c32f7e72b25d
Rogelio Guzman <chinog9@gmail.com>
2011-12-03 16:47:36 -0600

    Added coordinates to hardcoded geolocation

--- mpa_tiles.rb
+++ mpa_tiles.rb
@@ -4,14 +4,15 @@ require 'sinatra'
 require 'sqlite3'
 require 'haml'
 require 'geocoder'
+Geocoder::Configuration.lookup = :yahoo
 
 get '/tiles/:zoom/:column/:row' do
   content_type 'image/png'
-  
+
   # flip round the y coordinate for mapbox
   y = ((2**params[:zoom].to_f) - 1) - params[:row].to_f
-  
-  
+
+
   db = SQLite3::Database.new "data/Mexico.mbtiles"
   rows = db.execute("select images.tile_data 
                      from map 
@@ -20,23 +21,21 @@ get '/tiles/:zoom/:column/:row' do
                      where zoom_level = #{params[:zoom]} AND 
                      tile_row = #{y} AND 
                      tile_column = #{params[:column]}")
-  
-  rows[0][0] unless rows.empty?
+
+                     rows[0][0] unless rows.empty?
 end
 
 get '/' do
   haml :index
 end
 
-def geo_code query
-  Geocoder::Configuration.lookup = :yahoo
-  Geocoder.coordinates(query)
-end
 
 get '/json.json' do
   content_type 'application/json', :charset => 'utf-8'
-  ['Nuevo Leon', 'Sonora', 'Yucatan'].map do |state|
-    {:name => state, :attributes => {"Educación" => rand(40) + 10, "Salud" => rand(60) + 10 }}
+  ['Nuevo Leon', 'Sonora', 'Yucatan', 'DF'].map do |state|
+    x,y = Geocoder.coordinates("#{state}, Mexico")
+    {:name => state, :x => x, :y => y, :attributes => {"Educación" => rand(40) + 10, "Salud" => rand(60) + 10 }}
   end.to_json
 end
+
 
