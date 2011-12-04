8b5d2c5c2d17e69c621e967784a4ed80ae9c5716
Macario <macarui@gmail.com>
2011-12-04 00:01:52 -0600

    Wip

--- mpa_tiles.rb
+++ mpa_tiles.rb
@@ -15,7 +15,6 @@ get '/tiles/:zoom/:column/:row' do
   # flip round the y coordinate for mapbox
   y = ((2**params[:zoom].to_f) - 1) - params[:row].to_f
 
-
   db = SQLite3::Database.new "data/Mexico.mbtiles"
   rows = db.execute("select images.tile_data 
                      from map 
@@ -31,39 +30,29 @@ get '/' do
   haml :index
 end
 
-# DATA = ['Nuevo Leon', 'Sonora', 'Yucatan', 'DF'].map do |state|
-#   lat, lng = Geocoder.coordinates("#{state}, Mexico")
-#   geometry   = {:type => 'Point', :coordinates => [lat, lng]}
-#   properties = {"Educación" => rand(40) + 10, "Salud" => rand(60) + 10 }
-#   {:id => state, :type => 'Feature', :geometry => geometry, :properties => properties}
-# end
+DATA = ['Nuevo Leon', 'Sonora', 'Yucatan', 'DF'].map do |state|
+  lng, lat   = Geocoder.coordinates("#{state}, Mexico")
+  geometry   = {:type => 'Point', :coordinates => [lat, lng]}
+  properties = {"Educación" => rand(40) + 10, "Salud" => rand(60) + 10 }
+  {:id => state, :type => 'Feature', :geometry => geometry, :properties => properties}
+end
 
-DATA = [{:id=>"Nuevo Leon", :type=>"Feature", :geometry=>{:type=>"Point", :coordinates=>[25.7276624, -99.54509739999999]}, :properties=>{"Educación"=>26, "Salud"=>36}}, {:id=>"Sonora", :type=>"Feature", :geometry=>{:type=>"Point", :coordinates=>[29.2972247, -110.3308814]}, :properties=>{"Educación"=>28, "Salud"=>65}}, {:id=>"Yucatan", :type=>"Feature", :geometry=>{:type=>"Point", :coordinates=>[20.7098786, -89.0943377]}, :properties=>{"Educación"=>13, "Salud"=>11}}, {:id=>"DF", :type=>"Feature", :geometry=>{:type=>"Point", :coordinates=>[19.4326077, -99.133208]}, :properties=>{"Educación"=>19, "Salud"=>32}}]
+DATA = [
+  {:id=>"Nuevo Leon", :type=>"Feature", :geometry=>{:type=>"Point", :coordinates=>[-99.54509739999999, 25.7276624]}, :properties=>{"Educación"=>26, "Salud"=>36}}, 
+  {:id=>"Sonora", :type=>"Feature", :geometry=>{:type=>"Point", :coordinates=>[-110.3308814, 29.2972247]}, :properties=>{"Educación"=>28, "Salud"=>65}}, 
+  {:id=>"Yucatan", :type=>"Feature", :geometry=>{:type=>"Point", :coordinates=>[-89.0943377, 20.7098786]}, :properties=>{"Educación"=>13, "Salud"=>11}}, 
+  {:id=>"DF", :type=>"Feature", :geometry=>{:type=>"Point", :coordinates=>[-99.133208, 19.4326077]}, :properties=>{"Educación"=>19, "Salud"=>32}}
+]
 
 get '/json' do
   content_type 'application/json', :charset => 'utf-8'
 
   bb  = params[:bbox].split(',')
-  lat = Range.new(*[bb[0], bb[2]].map(&:to_f).sort)
-  lng = Range.new(*[bb[1], bb[3]].map(&:to_f).sort)
-
+  lng = Range.new(*[bb[0], bb[2]].map(&:to_f).sort)
+  lat = Range.new(*[bb[1], bb[3]].map(&:to_f).sort)
   data = DATA.select do |element|
     coordinates = element[:geometry][:coordinates]
     lat.include?(coordinates.first) && lng.include?(coordinates.last) 
   end
-
   {:type => 'FeatureCollection', :features => data}.to_json
 end
-
-get '/prueba' do
-  @name = 'Chino'
-  @attributes = ['Educación', 'Salud', 'Delitos', 'Arrestos', 'IPods robados']
-  @states = ['Nuevo Leon', 'Sonora', 'Yucatan', 'DF'].map do |state|
-    lat,long = Geocoder.coordinates("#{state}, Mexico")
-    {:name => state, :lat => lat, :long => long, :attributes => {"Educación" => rand(40) + 10, "Salud" => rand(60) + 10 }}
-  end
-  erb :prueba
-
-end
-
-
--- public/javascripts/app2.js
+++ public/javascripts/app2.js
@@ -20,21 +20,29 @@ map.add(po.geoJson()
 map.add(po.compass()
     .pan("none"));
 
+var setRadius = function(attribute) {
+  $('svg.map circle.point').each(function(){
+  
+  });
+  // var r = 1200 * Math.pow(2, e.tile.zoom - 12);
+  // point.setAttribute("r", r);
+}
+
 function load(e) {
-  var tile = e.tile, g = tile.element;
-  while (g.lastChild) g.removeChild(g.lastChild);
-
-  if (e.features.length > 0) {
-    console.log(e);
-  }
-
   for (var i = 0; i < e.features.length; i++) {
-    var data  = e.features[i].data;
-    var point = g.appendChild(po.svg("circle"));
-    var value = data.properties['Salud'];
+    var f = e.features[i],
+    c = f.element;
+    g = f.element = po.svg("g");
 
-    point.setAttribute("cx", data.geometry.coordinates[0]);
-    point.setAttribute("cy", data.geometry.coordinates[1]);
-    point.setAttribute("r", 5);
+    point = g.appendChild(po.svg("circle"));
+    point.setAttribute("cx", 0);
+    point.setAttribute("cy", 0);
+    point.setAttribute("class", "point")
+    point.setAttribute("r", 10);
+
+    g.setAttribute("transform", c.getAttribute("transform"));
+    g.setAttribute("style","cursor:pointer;");
+    g.setAttribute("data-name", f.data.id);
+    c.parentNode.replaceChild(g, c);
   }
 }
